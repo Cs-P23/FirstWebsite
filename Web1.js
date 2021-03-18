1c0e1fec58eda2deb5ad6a00d857fa813528eb20
christian <christian.spitler@cinteot.com>
2021-03-18 15:12:15 -0400

    made validation for SSN

--- Web1.js
+++ Web1.js
@@ -1,5 +1,17 @@
  
 
-function NewPage() {
-    location.href("whatscss.html")
+function validateSSN(){
+    var x = document.forms["yourSSN"]["SSN"].value;
+    if (x.length() != 8) {
+        window.alert("You didn't enter 8 digits. Please enter 8 digits so we can verify your identity");
+        return false;
+    }
+    // This should be an or statement, bc if it is 8 characters
+    // long, but with not digits, it will return true.
+    // Somehow make it an or
+    else if (x.typeof()==='string') {
+        window.alert("You accidentally typed something other than digits. Please re-enter your SSN properly.");
+        return false;
+    }
+    
 }
